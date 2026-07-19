5d40cf2c6777bf29c342ff90884129996427f16c
Cai Xubin <caixubin1010@gmail.com>
2026-07-19 23:58:39 +0800

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        t= ""
+        for char in s:
+            if char.isalnum():
+                t+= char.lower()
+                
+        l,r= 0, len(t)-1
+        while l<r:
+            if t[l]!=t[r]:
+                return False
+            l+=1
+            r-=1
+        return True
